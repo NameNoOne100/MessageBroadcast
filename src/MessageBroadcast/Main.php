37430332622fa00834cb6ac1c5e6b723145faef7
NameNoOne100 <morganewing1@icloud.com>
2016-01-09 20:35:40 +1100

    Update Main.php

--- src/MessageBroadcast/Main.php
+++ src/MessageBroadcast/Main.php
@@ -4,3 +4,21 @@
 
   use pocketmine\plugin\PluginBase;
   use pocketmine\event\Listener;
+  use pocketmine\command\Command;
+  use pocketmine\command\CommandSender;
+  use pocketmine\command\CommandExecutor;
+
+  class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+
+      if($cmd->getName()) === "broadcast") {
+
+        $msg = implode(" ", $args);
+        $this->getServer()->broadcastMessage($msg);
